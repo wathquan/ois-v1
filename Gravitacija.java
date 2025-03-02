a90ccc4b5efe7b5020a418a3ab1d2f3dd143e394
wathquan <anze.rakovec@gmail.com>
2025-03-02 20:03:14 +0100

    Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,8 +2,17 @@ import java.util.*;
 
 public class Gravitacija {
 
-    public static void main(String[] args) {
+    public static final double GRAVITACIJSKA_KONSTANTA = 6.674 * Math.pow(10, -11);
+    public static final double MASA_ZEMLJE = 5.972 * Math.pow(10, 24);
+    public static final double POLMER_ZEMLJE = 6.371 * Math.pow(10, 6);
 
-        System.out.println("OIS je zakon!");
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        double visina = sc.nextDouble();
+        // Student B doda staticno double metodo izracunaj(visina)
+        // Student C doda void metodo izpis()
+        double gravPospesek = izracunaj(visina);
+        izpis(gravPospesek);
     }
 }
